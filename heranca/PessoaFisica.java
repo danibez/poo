c811135af94152b5fe6a19bacbb904fa4a65186e
Daniel Bezerra <daniel.bezerra92@gmail.com>
2026-03-13 23:12:25 +0000

    aplicacao com polimorfismo

--- heranca/PessoaFisica.java
+++ heranca/PessoaFisica.java
@@ -13,4 +13,24 @@ public class PessoaFisica extends Pessoa {
         return super.getNome();
     }
 
+    public int getIdade() {
+        return super.getIdade();
+    }
+
+    public void setIdade(int idade) {
+        super.setIdade(idade);
+    }
+    
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
 }
