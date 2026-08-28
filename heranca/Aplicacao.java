6970143a6ea9b3738316f080ea3a21c75473b95f
Daniel Bezerra <daniel.bezerra92@gmail.com>
2026-08-28 23:07:10 +0000

    ajustes na aplicacao

--- heranca/Aplicacao.java
+++ heranca/Aplicacao.java
@@ -3,29 +3,28 @@ import java.util.Scanner;
 public class Aplicacao {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String nome, info;
-        int idade;
+        String nome, endereco, info;
 
         nome = s.next();
-        idade = s.nextInt();
+        endereco = s.next();
         info = s.next();
-        Pessoa pf = new PessoaFisica(nome, idade, info);
+        ClienteHeranca pf = new ClientePF(nome, endereco, info);
         imprime(pf);
 
         nome = s.next();
-        idade = s.nextInt();
+        endereco = s.next();
         info = s.next();
-        Pessoa pj = new PessoaJuridica(nome, idade, info);
+        ClienteHeranca pj = new ClientePJ(nome, endereco, info);
         imprime(pj);
     }
 
-    public static void imprime(Pessoa p){
+    public static void imprime(ClienteHeranca p){
         System.out.println("Nome da Pessoa: " + p.getNome());
-        System.out.println("Idade da Pessoa: " + p.getIdade());
-        if(p instanceof PessoaFisica)
-            System.out.println("CPF da Pessoa: " + ((PessoaFisica)p).getCpf() );
+        System.out.println("Endereco da Pessoa: " + p.getEndereco());
+        if(p instanceof ClientePF)
+            System.out.println("CPF da Pessoa: " + ((ClientePF)p).getCpf() );
         else
-            System.out.println("CNPJ da Pessoa: " + ((PessoaJuridica)p).getCnpj() );
+            System.out.println("CNPJ da Pessoa: " + ((ClientePJ)p).getCnpj() );
     }
 
 }
